bdb7b966c1f5f63ac77753f264f51d8fe1b17e3d
paoloanchoriz <paolo.anchoriz@gmail.com>
2015-06-23 22:14:42 +0800

    Added something

---
+++ src/main/webapp/resources/js/controllers/gameScheduleController.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+var gameScheduleController = angular.module('gameScheduleController', []);
+
+	gameScheduleController.controller('GameScheduleFormController', 
+			['$scope', '$routeParams', '$location', 'Constants',
+			 function($scope, $routeParams, $location, Constants) {
+				
+			}]);
